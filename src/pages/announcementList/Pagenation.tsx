946a59ca680a203239c2993b21fb51b3f67eeb5f
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-04-16 14:34:00 +0900

    test: 테일윈트 color 객체 테스트

---
+++ src/pages/announcementList/Pagenation.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Pagenation = () => {
+  return (
+    <div>
+      <span className="text-Red-20">color test</span>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
+    </div>
+  );
+};
+
+export default Pagenation;
