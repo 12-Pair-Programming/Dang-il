81fe0f370f62229edec7e8eeb5e729ec3ca10503
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-04-17 20:20:59 +0900

    feat: PageButton, ButtonWrapper 컴포넌트 구현

---
+++ shared/@common/ui/pagination/ButtonWrapper.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react';
+
+const ButtonWrapper = ({ children }: { children: ReactNode }) => {
+  return <div className="flex align-center gap-[20px]"></div>;
+};
+
+export default ButtonWrapper;
--- shared/@common/ui/pagination/Pagination.tsx
+++ shared/@common/ui/pagination/Pagination.tsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import PaginationWrapper from './PaginationWrapper';
+import FaAngleLeft from './FaAngleLeft';
+import FaAngleRight from './FaAngleRight';
+import ButtonWrapper from './ButtonWrapper';
+import PageButton from './PageButton';
 
 interface PaginationProps {
   totalPage: number;
@@ -10,7 +14,7 @@ interface PaginationProps {
 
 const Pagination = ({ totalPage, limit, page, setPage }: PaginationProps) => {
   const [currentPageArray, setCurrentPageArray] = useState([]);
-  const [totalPageArray, setTotalPageArray] = useState<>([]);
+  const [totalPageArray, setTotalPageArray] = useState([]);
   const sliceArrayByLimit = (totalPageArray, limit) => {
     totalPageArray.slice(0, limit);
   };
@@ -30,7 +34,28 @@ const Pagination = ({ totalPage, limit, page, setPage }: PaginationProps) => {
     setCurrentPageArray(slicedPageArray[0]);
   }, [totalPage]);
 
-  return <PaginationWrapper></PaginationWrapper>;
+  return (
+    <PaginationWrapper>
+      <ButtonWrapper>
+        <FaAngleLeft onClick={() => setPage(page - 1)} disabled={page === 1} />
+
+        {currentPageArray?.map((i) => (
+          <PageButton
+            key={i + 1}
+            onClick={() => setPage(i + 1)}
+            aria-current={page === i + 1 ? 'page' : null}
+          >
+            {i + 1}
+          </PageButton>
+        ))}
+
+        <FaAngleRight
+          onClick={() => setPage(page + 1)}
+          disabled={page === totalPage}
+        />
+      </ButtonWrapper>
+    </PaginationWrapper>
+  );
 };
 
 export default Pagination;
