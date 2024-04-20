5dec5d8d9696b6788c6ee970e4417ea391873f4b
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-04-20 20:00:12 +0900

    refactor: 페이지네이션 바 불필요한 코드 제거

--- pages/test/paginationBar.tsx
+++ pages/test/paginationBar.tsx
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 import Pagination from '../../shared/@common/ui/Pagination/Pagination';
 
 const PaginationBar = () => {
-  const [page, setPage] = useState(0);
   return (
     <>
-      <Pagination totalPage={21} limit={7} page={page} setPage={setPage} />
+      <Pagination totalPage={21} limit={7} />
     </>
   );
 };
