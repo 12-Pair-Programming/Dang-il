34b3d2313c1a47a37455a3073e0640c509c6f3d4
이진호 <tead7@naver.com>
2024-04-22 20:36:42 +0900

    fix: 인풋 파라미터 및 훅 변경

--- shared/@common/ui/Input/Input.tsx
+++ shared/@common/ui/Input/Input.tsx
@@ -2,18 +2,19 @@ import React, { useState } from 'react';
 import Image from 'next/image';
 
 interface InputProps {
-  title: string;
+  title?: string;
   placeholder: string;
   type?: string;
   width?: string;
   isError?: boolean;
   errorText?: string;
   countText?: string;
-  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
 }
 
 export const Input = ({
-  title = '제목을 입력해주세요.',
+  title,
   placeholder = '값을 입력해주세요.',
   type = 'text',
   width = '350px',
@@ -21,6 +22,7 @@ export const Input = ({
   errorText,
   countText,
   onChange,
+  onBlur,
 }: InputProps) => {
   const firstType = type;
   const [inputType, setInputType] = useState(type);
@@ -30,42 +32,43 @@ export const Input = ({
   };
 
   return (
-    <>
-      <div className={`w-[${width}] flex flex-col items-start gap-2`}>
+    <div className={`w-[${width}] flex flex-col items-start gap-2`}>
+      {title && (
         <p className="text-black font-sans font-normal text-base leading-6">
           {title}
         </p>
-        <div className={`w-[${width}] relative`}>
-          <input
-            className={`flex py-4 px-5 w-full rounded-md border ${
-              isError ? 'border-red-40' : 'border-gray-30'
-            } bg-white items-start gap-2 self-stretch`}
-            type={inputType}
-            placeholder={placeholder}
-            onChange={onChange}
+      )}
+      <div className={`w-[${width}] relative`}>
+        <input
+          className={`flex py-4 px-5 w-full rounded-md border ${
+            isError ? 'border-red-40' : 'border-gray-30'
+          } focus:outline-none focus:border-purple-40 bg-white items-start gap-2 self-stretch`}
+          type={inputType}
+          placeholder={placeholder}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+        {firstType === 'password' && (
+          <Image
+            className={`absolute top-1/2 right-4 transform -translate-y-1/2 w-4 h-4 flex-shrink-0`}
+            width={16}
+            height={16}
+            src={
+              inputType === 'password'
+                ? 'images/eye-off.svg'
+                : 'images/eye-on.svg'
+            }
+            alt={'눈 표시'}
+            onClick={handleEye}
           />
-          {firstType === 'password' && (
-            <Image
-              className={`absolute top-1/2 right-4 transform -translate-y-1/2 w-4 h-4 flex-shrink-0`}
-              width={16}
-              height={16}
-              src={
-                inputType === 'password'
-                  ? 'images/eye-off.svg'
-                  : 'images/eye-on.svg'
-              }
-              alt={'눈 표시'}
-              onClick={handleEye}
-            />
-          )}
-          {countText && (
-            <p className="h-[24px] absolute top-1/2 right-4 transform -translate-y-1/2 w-4  flex-shrink-0">
-              {countText}
-            </p>
-          )}
-        </div>
-        {isError && <p className="text-red-500 pl-2 text-xs">{errorText}</p>}
+        )}
+        {countText && (
+          <p className="h-[24px] absolute top-1/2 right-4 transform -translate-y-1/2 w-4  flex-shrink-0">
+            {countText}
+          </p>
+        )}
       </div>
-    </>
+      {isError && <p className="text-red-500 pl-2 text-xs">{errorText}</p>}
+    </div>
   );
 };
