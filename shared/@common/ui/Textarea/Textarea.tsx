f4ae40ea54fb42ba83a61a3442a0598fe6ef3279
이진호 <tead7@naver.com>
2024-04-22 21:16:42 +0900

    feat: 텍스트 에리어 추가

---
+++ shared/@common/ui/Textarea/Textarea.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+
+interface TextareaProps {
+  title: string;
+  placeholder: string;
+  width?: string;
+  height?: string;
+  isError?: boolean;
+  errorText?: string;
+  countText?: string;
+  onChange?: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+  onBlur?: (event: React.FocusEvent<HTMLTextAreaElement>) => void;
+}
+
+export const Textarea = ({
+  title = '제목을 입력해주세요.',
+  placeholder = '값을 입력해주세요.',
+  width = '350px',
+  height = 'auto',
+  isError = false,
+  errorText,
+  countText,
+  onChange,
+  onBlur,
+}: TextareaProps) => {
+  return (
+    <div className={`w-auto h-auto flex flex-col items-start gap-2`}>
+      <p className="text-black font-sans font-normal text-base leading-6">
+        {title}
+      </p>
+      <div
+        className={`relative`}
+        style={{ width: `${width}`, height: `${height}` }}
+      >
+        <textarea
+          className={`flex py-4 px-5 w-full h-full rounded-md border ${
+            isError ? 'border-red-40' : 'border-gray-30'
+          } focus:outline-none focus:border-purple-40 bg-white items-start gap-2 self-stretch`}
+          placeholder={placeholder}
+          onChange={onChange}
+          onBlur={onBlur}
+        />
+        {countText && (
+          <p className="h-[24px] absolute top-1/2 right-4 transform -translate-y-1/2 w-4  flex-shrink-0">
+            {countText}
+          </p>
+        )}
+      </div>
+      {isError && <p className="text-red-500 pl-2 text-xs">{errorText}</p>}
+    </div>
+  );
+};
---
+++ shared/@common/ui/Textarea/hook/textareaHook.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+
+export const useTextarea = (defaultValue?: string) => {
+  const [value, setValue] = useState(defaultValue || '');
+
+  const handleTextarea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setValue(e.target.value);
+  };
+
+  return { handleTextarea, value };
+};
+
+/* 
+훅 사용법
+const { handleInput, value } = inputHook({ inputValue: '' }); 값지정
+<Input type="text" onChange={handleInput} /> 인풋 지정
+handleInput<= 이거에 따라 값이 지정되는 곳이 달라집니다.
+*/
