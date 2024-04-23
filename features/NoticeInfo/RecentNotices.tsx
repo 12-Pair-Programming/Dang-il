43d0c8803ccb4c86cf5438b774241ffe23459620
Yeojin Yoon <roseiris1001@gmail.com>
2024-04-23 14:10:49 +0900

    feat: 공고 상세 페이지 생성

---
+++ features/NoticeInfo/RecentNotices.tsx
@@ -0,0 +1,86 @@
+import Card from '@/shared/@common/ui/Card/Card';
+
+/**
+ * 최근에 본 공고 리스트 컴포넌트
+ * @returns
+ */
+const RecentNotices = () => {
+  console.log('Recent Notices');
+  //TODO:
+  //6개만 보여줘야 함
+  //데이터 직접 연결 필요
+
+  return (
+    <div className="w-[963px] mt-[60px] mb-[12px] mx-[238px]">
+      <div>
+        <p className="text-[28px] font-bold">최근에 본 공고</p>
+      </div>
+      <div className="mt-8 flex gap-[14px]">
+        <Card
+          name="시급 비싼 가게"
+          imageUrl="https://img.sbs.co.kr/newsnet/etv/upload/2022/09/19/30000790950.jpg"
+          address1="서울시 강남구"
+          startsAt="2023-07-07T18:00:00.000Z"
+          workhour={12}
+          hourlyPay={100000}
+          originalHourlyPay={10000}
+          closed={false}
+        />
+        <Card
+          name=" 시급 동일 가게"
+          imageUrl="https://img.sbs.co.kr/newsnet/etv/upload/2022/09/19/30000790950.jpg"
+          address1="서울시 강남구"
+          startsAt="2023-07-07T18:00:00.000Z"
+          workhour={12}
+          hourlyPay={1000}
+          originalHourlyPay={1000}
+          closed={false}
+        />
+        <Card
+          name="시급 낮은 가게"
+          imageUrl="https://img.sbs.co.kr/newsnet/etv/upload/2022/09/19/30000790950.jpg"
+          address1="서울시 강남구"
+          startsAt="2023-07-07T18:00:00.000Z"
+          workhour={12}
+          hourlyPay={100}
+          originalHourlyPay={1000}
+          closed={false}
+        />
+      </div>
+      <div className="mt-8 flex gap-[14px]">
+        <Card
+          name="시급 비싼 가게"
+          imageUrl="https://img.sbs.co.kr/newsnet/etv/upload/2022/09/19/30000790950.jpg"
+          address1="서울시 강남구"
+          startsAt="2023-07-07T18:00:00.000Z"
+          workhour={12}
+          hourlyPay={100000}
+          originalHourlyPay={10000}
+          closed={true}
+        />
+        <Card
+          name=" 시급 동일 가게"
+          imageUrl="https://img.sbs.co.kr/newsnet/etv/upload/2022/09/19/30000790950.jpg"
+          address1="서울시 강남구"
+          startsAt="2023-07-07T18:00:00.000Z"
+          workhour={12}
+          hourlyPay={1000}
+          originalHourlyPay={1000}
+          closed={true}
+        />
+        <Card
+          name="시급 낮은 가게"
+          imageUrl="https://img.sbs.co.kr/newsnet/etv/upload/2022/09/19/30000790950.jpg"
+          address1="서울시 강남구"
+          startsAt="2023-07-07T18:00:00.000Z"
+          workhour={12}
+          hourlyPay={100}
+          originalHourlyPay={1000}
+          closed={true}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default RecentNotices;
