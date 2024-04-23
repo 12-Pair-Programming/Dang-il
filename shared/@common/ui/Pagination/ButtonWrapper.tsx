4879feb06627c05a30eaad0b9b5534a212933ffe
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-04-23 17:34:42 +0900

    fix: 깃허브 conflict 해결

--- shared/@common/ui/Pagination/ButtonWrapper.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from 'react';
-
-const ButtonWrapper = ({ children }: PropsWithChildren) => {
-  return (
-    <div className="flex justify-between align-center h-10">{children}</div>
-  );
-};
-
-export default ButtonWrapper;
