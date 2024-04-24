21ce2d5e268aa62689351b49050e9e654c4054d8
Yeojin Yoon <roseiris1001@gmail.com>
2024-04-24 15:22:44 +0900

    feat: 공고 상세 페이지 - 사장님, 사용자 분리

---
+++ features/NoticeInfo/EmployeeList.tsx
@@ -0,0 +1,14 @@
+const EmployeeList = () => {
+  return (
+    <div className="w-[963px] mt-[60px] mb-3 mx-[238px]">
+      <div>
+        <p className="text-[28px] font-bold">신청자 목록</p>
+      </div>
+      <div className="mt-8 flex gap-[14px]">
+        <p>리스트들</p>
+      </div>
+    </div>
+  );
+};
+
+export default EmployeeList;
--- pages/noticeInfo.tsx
+++ pages/noticeInfo.tsx
@@ -1,21 +1,78 @@
 import ShopInfo from '@/features/NoticeInfo/ShopInfo';
 import RecentNotices from '@/features/NoticeInfo/RecentNotices';
+import EmployeeList from '@/features/NoticeInfo/EmployeeList';
 
 /**
  * 공고 상세 페이지 컴포넌트
  * @returns
  */
 const NoticeInfo = () => {
-  //TODO: 데이터 받아서 각 컴포넌트에 전달 필요
-  //1. 로그인 정보가 있는 경우
-  //  - 신청하기 버튼 동작
-  //  - 프로필이 없는 경우 신청하기 버튼에서 모달이 떠야 함
-  //  - 취소하기 버튼 동작
-  //  - 로그인 정보가 없는 경우 신청하기 버튼에서 모달이 떠야 함
+  // TODO: 로그인 정보 연결 필요
+  const userType = 'employer'; //user.item.type : employer | employee;
+  const isLogin = true;
+
+  //TODO: 목업 데이터 api연결해서 받아와야 함
+
+  const item = {
+    id: 'string',
+    hourlyPay: 30000,
+    startsAt: '2023-07-07T18:00:00.000Z',
+    workhour: 5,
+    description:
+      '기존 알바 친구가 그만둬서 새로운 친구를 구했는데, 그 사이에 하루가 비네요. 급해서 시급도 높였고 그렇게 바쁜 날이 아니라서 괜찮을거예요.',
+    closed: false,
+    shop: {
+      item: {
+        id: 'string',
+        name: 'string',
+        category: 'string',
+        address1: '서울시 영등포구',
+        address2: 'string',
+        description:
+          '알바하기 편한 너구리네 라면집! 라면 올려두고 끓이기만 하면 되어서 쉬운 편에 속하는 가게입니다.',
+        imageUrl:
+          'https://search.pstatic.net/common/?src=http%3A%2F%2Fblogfiles.naver.net%2FMjAxODA4MDlfMzkg%2FMDAxNTMzNzc0NDQzMjM0.2tTaXYEZ5qoD4tSKJb1b_RReTCNCgCdYhSKO52GpxKog.0JV9pl0bPnDRZhEkAG3YAzOyu5_d3CpH4p4uQ2ciCZYg.JPEG.gyubin0804%2F1533774357467.jpg&type=sc960_832',
+        originalHourlyPay: 1000,
+      },
+      href: 'string',
+    },
+    currentUserApplication: {
+      item: {
+        id: 'string', // application.id,
+        status: 'pending | accepted | rejected | canceled', // application.status
+        createdAt: 'string', // application.createdAt
+      },
+    },
+  };
+
+  const closed = item.closed;
+  const status = item.currentUserApplication.item.status;
+  const hourlyPay = item.hourlyPay;
+  const address1 = item.shop.item.address1;
+  const originalHourlyPay = item.shop.item.originalHourlyPay;
+  const startsAt = item.startsAt;
+  const workhour = item.workhour;
+  const shopDescription = item.shop.item.description;
+  const noticeDescription = item.description;
+  const shopImageUrl = item.shop.item.imageUrl;
+
   return (
     <div className="bg-white text-black">
-      <ShopInfo />
-      <RecentNotices />
+      <ShopInfo
+        userType={userType}
+        isLogin={isLogin}
+        closed={closed}
+        status={status}
+        hourlyPay={hourlyPay}
+        address1={address1}
+        originalHourlyPay={originalHourlyPay}
+        startsAt={startsAt}
+        workhour={workhour}
+        shopDescription={shopDescription}
+        noticeDescription={noticeDescription}
+        shopImageUrl={shopImageUrl}
+      />
+      {userType === 'employer' ? <EmployeeList /> : <RecentNotices />}
     </div>
   );
 };
