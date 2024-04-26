4980d600637d464320c789a1ea9e628c6a230992
이진호 <tead7@naver.com>
2024-04-26 22:02:15 +0900

    style: 모달 반응형 추가

--- shared/@common/ui/Modal/modalType/noticeModal.tsx
+++ shared/@common/ui/Modal/modalType/noticeModal.tsx
@@ -8,13 +8,13 @@ interface NoticeModalProps {
 
 export const NoticeModal = ({ content, closeModal }: NoticeModalProps) => {
   return (
-    <div className="w-[540px] h-[250px] flex flex-col items-center justify-center p-6">
+    <div className="w-[540px] h-[250px] flex flex-col items-center justify-center p-6 mobile:w-[327px]">
       <div className="flex flex-col items-center justify-center gap-4 h-full">
         <p className="font-Pretendard font-medium text-base text-center">
           {content}
         </p>
       </div>
-      <div className="flex justify-end w-full">
+      <div className="flex justify-end w-full mobile:justify-center">
         <Button size="medium" color="colored" onClick={closeModal}>
           확인
         </Button>
