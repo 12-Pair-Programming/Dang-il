8131fc9421e5b0e642a057dc098e397b7e253239
이진호 <tead7@naver.com>
2024-04-28 01:35:56 +0900

    fix: 풋터 반응형 디자인 수정

--- shared/@common/ui/Footer/Footer.tsx
+++ shared/@common/ui/Footer/Footer.tsx
@@ -4,8 +4,8 @@ import Link from 'next/link';
 
 const Footer = () => {
   return (
-    <footer className="w-full px-[238px] py-8 flex flex-col bg-gray-10 gap-2 tablet:px-8 tablet:py-[37px] mobile:px-5 pt-8 pb-4">
-      <div className="flex self-stretch justify-between flex-row mobile:grid mobile:grid-cols-2 mobile:gap-y-10">
+    <footer className="w-full h-[150px] px-[238px] py-8 flex flex-col justify-center items-center bg-gray-10 gap-2 tablet:px-8 tablet:py-[37px] mobile:px-5 pt-8 pb-4">
+      <div className="w-[1024px] flex justify-between flex-row mobile:grid mobile:grid-cols-2 mobile:gap-y-10">
         <div className="flex flex-row mobile:order-3 mobile:justify-start">
           <p className=" text-gray-50">©codeit - 2023</p>
         </div>
