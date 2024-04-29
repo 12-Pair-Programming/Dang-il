f79f0f957723d1fbfe960a0f03c3f8d1052bd610
Seoin <144193370+Seoin02@users.noreply.github.com>
2024-04-29 14:28:48 +0900

    style: noticeList 페이지 반응형 수정

--- features/NoticeList/CustomNotice.tsx
+++ features/NoticeList/CustomNotice.tsx
@@ -13,7 +13,6 @@ const CustomNotice = () => {
     const decodedToken = token ? jwtDecode(token) : null;
     const user = localStorage.getItem('user');
     const userId = (decodedToken as any)?.userId || '';
-
     const [noticeData, setNoticeData] = useState<Data>();
     const [address, setAddress] = useState('');
 
@@ -32,10 +31,10 @@ const CustomNotice = () => {
     const userAddress = data && data.item.address;
     console.log(noticeData);
     return (
-      <div className="flex w-full py-[60px] pc: px-auto flex-col items-center bg-purple-10 tracking-wide tablet:pl-8 tablet:items-start tablet:overflow-x-scroll tablet:whitespace-pre tablet:scrollbar-hide mobile:pl-3 mobile:overflow-x-scroll mobile:whitespace-pre mobile:scrollbar-hide mobile:items-start">
-        <div className="flex flex-col gap-[31px] px-4">
+      <div className="flex w-full py-[60px] px-auto flex-col items-center bg-purple-10">
+        <div className="flex flex-col gap-10 px-4 w-[983px] tablet:w-[678px] mobile:max-w-[520px]">
           <div className="text-[28px] font-bold">맞춤 공고</div>
-          <div className="flex gap-4">
+          <div className="w-[983px] tablet:w-[678px] mobile:max-w-[520px] flex items-start overflow-x-scroll scrollbar-hide gap-4">
             {userAddress && noticeData
               ? noticeData.items.length > 0 &&
                 noticeData.items
@@ -47,6 +46,7 @@ const CustomNotice = () => {
                   .map((item: ItemData) => (
                     <Link
                       href={`/noticeInfo?shopId=${item.item.shop.item.id}&noticeId=${item.item.id}`}
+                      className="max-w-[310px] mobile:max-w-[236px]"
                     >
                       <Card
                         name={item.item.shop.item.name}
